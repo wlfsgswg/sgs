d8f66d09b0a3390b1d6732d127c197c0951681a2
王六锋 <2496436621@qq.com>
2019-11-24 11:07:41 +0800

    no message

--- src/container/Test/index.jsx
+++ src/container/Test/index.jsx
@@ -175,6 +175,7 @@ class Test extends Component {
     }
     // 对treasureArr 数据进行处理，相同项合并
     // 祈福十次额外必得至少一个同心结
+
     if (active === "1" && int === 10) {
       // 首次模拟出一定额度同心结
       const firstTxj = randomNumber(0, 9);
