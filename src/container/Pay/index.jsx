1fc8d92e749ba27f6d9901e75bdf68c6707cfd7c
王六锋 <2496436621@qq.com>
2019-12-19 17:46:12 +0800

    no message

--- src/container/Pay/index.jsx
+++ src/container/Pay/index.jsx
@@ -1,50 +1,17 @@
 import React, { Component } from "react";
 import "./index.less";
-import { getToken, formatMoney } from "./../Test/fun/index.js";
-import pay from "./pay.png";
 class Record extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      json: [],
-      total: 0
-    };
+    this.state = {};
   }
 
-  componentDidMount() {
-    const json = JSON.parse(getToken()) ? JSON.parse(getToken()) : [];
-    let tal = 0;
-    if (json.length) {
-      json.map(it => {
-        tal = tal + it.pay;
-      });
-    }
-    this.setState({ json, total: tal });
-  }
+  componentDidMount() {}
 
   render() {
-    const { json, total } = this.state;
     return (
       <div className="sgs-pay">
-        <div className="sgs-pay-content">
-          {json.length === 0 ? (
-            <div className="my-img">
-              <img src={pay} alt="" />
-              <div className="title">暂无花费，先去模拟试试</div>
-            </div>
-          ) : (
-            <div>
-              <div className="record-bottom">
-                <div className="l-left">
-                  等值人民币
-                  <span style={{ color: "#00a4fa", fontSize: "18px" }}>
-                    {formatMoney(total / 100)}
-                  </span>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
+        <div className="sgs-pay-content">222</div>
       </div>
     );
   }
