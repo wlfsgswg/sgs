e9a55714579e8700c5efe626349dc7aa54158594
王六锋 <2496436621@qq.com>
2019-12-23 17:02:35 +0800

    no message

---
+++ src/components/Diy/SelfRadio/index.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { Radio, List } from "antd-mobile";
+import "./index.less";
+const RadioItem = Radio.RadioItem;
+
+class SelfRadio extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // 体力改变函数
+  handlePhysical(e) {
+    this.props.onChangePhy(e);
+  }
+  // 势力改变函数
+  handleCountry(e) {
+    this.props.onChangeCou(e);
+  }
+
+  render() {
+    const { physical, country } = this.props;
+    return (
+      <div className="sgs-diy-selfradio">
+        <div style={{ backgroundColor: "#f5f5f9", paddingBottom: "10px" }}>
+          <div style={{ backgroundColor: "#fff" }}>
+            <List renderHeader={() => "选择武将体力（4体力模板暂不开放）"}>
+              <RadioItem
+                key={3}
+                checked={physical === 3}
+                onChange={() => this.handlePhysical(3)}
+              >
+                3体力（阴阳鱼）
+              </RadioItem>
+              <RadioItem
+                key={4}
+                checked={physical === 4}
+                onChange={() => this.handlePhysical(4)}
+                disabled={true}
+              >
+                4体力（阴阳鱼）
+              </RadioItem>
+            </List>
+          </div>
+        </div>
+        <div
+          style={{ backgroundColor: "#f5f5f9", paddingBottom: "10px" }}
+          className="list2"
+        >
+          <div style={{ backgroundColor: "#fff" }}>
+            <List renderHeader={() => "选择武将势力"}>
+              <RadioItem
+                key={1}
+                checked={country === 1}
+                onChange={() => this.handleCountry(1)}
+              >
+                魏
+              </RadioItem>
+              <RadioItem
+                key={2}
+                checked={country === 2}
+                onChange={() => this.handleCountry(2)}
+              >
+                蜀
+              </RadioItem>
+              <RadioItem
+                key={3}
+                checked={country === 3}
+                onChange={() => this.handleCountry(3)}
+              >
+                吴
+              </RadioItem>
+              <RadioItem
+                key={4}
+                checked={country === 4}
+                onChange={() => this.handleCountry(4)}
+              >
+                群
+              </RadioItem>
+              <RadioItem
+                key={5}
+                checked={country === 5}
+                onChange={() => this.handleCountry(5)}
+              >
+                神
+              </RadioItem>
+            </List>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SelfRadio;
--- src/components/Diy/index.jsx
+++ src/components/Diy/index.jsx
@@ -1,16 +1,65 @@
 import React from "react";
+import { ImagePicker, List } from "antd-mobile";
+import SelfRadio from "./SelfRadio/index.jsx";
 import "./index.less";
-
 class Diy extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      files: [],
+      //武将体力
+      physical: 3,
+      //武将势力
+      country: 1
+    };
   }
 
+  handleChange = files => {
+    this.setState({ files });
+  };
+
   render() {
+    const { files, physical, country } = this.state;
     return (
-      <div>
-        <div>1</div>
-        <div>1</div>
+      <div className="sgs-component-diy">
+        <div className="sgs-component-diy-menu">
+          <div className="top">DIY三国杀武将</div>
+        </div>
+        <SelfRadio
+          physical={physical}
+          country={country}
+          onChangePhy={e => this.setState({ physical: e })}
+          onChangeCou={e => this.setState({ country: e })}
+        />
+        <div style={{ backgroundColor: "#f5f5f9", paddingBottom: "10px" }}>
+          <div style={{ backgroundColor: "#fff" }}>
+            <List renderHeader={() => "从手机中选择一张背景图"}>
+              {files.length !== 0 ? (
+                <div className="menu-div">
+                  <div className="diy-div">
+                    <img src={files[0].url} alt="" />
+                    <div
+                      className="pos"
+                      onClick={() => this.setState({ files: [] })}
+                    >
+                      更改图片
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <ImagePicker
+                  files={files}
+                  onChange={this.handleChange}
+                  onImageClick={(index, fs) => console.log(index, fs)}
+                  selectable={1}
+                  multiple={false}
+                  length={2}
+                  disableDelete={true}
+                />
+              )}
+            </List>
+          </div>
+        </div>
       </div>
     );
   }
