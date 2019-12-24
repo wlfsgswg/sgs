35da132912d261615ce7705d37c7880d9bb0eddb
王六锋 <2496436621@qq.com>
2019-12-24 15:12:16 +0800

    no message

--- src/container/Test/index.jsx
+++ src/container/Test/index.jsx
@@ -346,6 +346,7 @@ class Test extends Component {
             祈福模拟只为单纯测试，和实际开箱可能会有出入，建议慎重氪金，小编不承担额外责任
           </div>
           <div className="sgs-component-muban-font">甘宁</div>
+          <div className="sgs-component-muban-font2">甘宁</div>
         </div>
       </div>
     );
