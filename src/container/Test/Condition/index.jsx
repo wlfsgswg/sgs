08b34a81077494bae4ffaf53ce2fb974cbf095eb
王六锋 <2496436621@qq.com>
2020-01-17 13:58:08 +0800

    no message

--- src/container/Test/Condition/index.jsx
+++ src/container/Test/Condition/index.jsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { Carousel, WingBlank, Tabs, NoticeBar } from "antd-mobile";
 import "./index.less";
 const Sgs1 =
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577340722258&di=81fa289093e4ac22a1324af0783a77aa&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fphotoblog%2F1305%2F18%2Fc0%2F21057146_21057146_1368849752640.jpg";
+  "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3723280583,1768992622&fm=26&gp=0.jpg";
 const Sgs2 =
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577340722258&di=ee3f7344a588f22fcf09959c32548e6d&imgtype=0&src=http%3A%2F%2Fdpic.tiankong.com%2F3g%2F2m%2FQJ6193278952.jpg%3Fx-oss-process%3Dstyle%2Fshow";
+  "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2383279309,161228295&fm=26&gp=0.jpg";
 const Sgs3 =
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577340722257&di=ace5c012812b77370d362e75f77ca85b&imgtype=0&src=http%3A%2F%2Fk.zol-img.com.cn%2Fdcbbs%2F20054%2Fa20053327_01000.jpg";
+  "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2376780534,900408061&fm=26&gp=0.jpg";
 const Sgs4 =
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577340722256&di=29190f99f5cd6ad415b91ff8f0d7622b&imgtype=0&src=http%3A%2F%2Fi5.3conline.com%2Fimages%2Fpiclib%2F201001%2F05%2Fbatch%2F1%2F51294%2F1262701836620fbqy1kdqi4.jpg";
+  "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1248731800,4059826141&fm=26&gp=0.jpg";
 class Condition extends Component {
   constructor(props) {
     super(props);
