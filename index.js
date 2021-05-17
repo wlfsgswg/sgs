e5e33b871ab486360f93f66655d887f438a7db5f
王六锋 <2496436621@qq.com>
2021-05-17 14:11:05 +0800

    no message

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./src/index.jsx";
-
 if (module.hot) {
   module.hot.accept("./src/index.jsx", () => {
     ReactDom.render(<App />, document.getElementById("app"));
